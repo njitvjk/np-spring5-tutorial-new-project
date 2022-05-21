2634cd4f6a75f8d2221981228bf36b1b9cb1c481
njitvjk <kv332@njit.edu>
2022-05-21 16:28:53 -0400

    Initial commit

---
+++ src/main/java/com/example/npspring5tutorialnewproject/NpSpring5TutorialNewProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.npspring5tutorialnewproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpSpring5TutorialNewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpSpring5TutorialNewProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/npspring5tutorialnewproject/NpSpring5TutorialNewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.npspring5tutorialnewproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpSpring5TutorialNewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
